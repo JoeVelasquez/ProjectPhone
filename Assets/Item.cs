5253ed0a21393fdcc751a39b6b4835b767b0bb89
Tobirenn <37786386+Tobirenn@users.noreply.github.com>
2018-04-08 20:28:37 -0500

    Coding
    I put some code but I commented them for now since I need to adjust them once we put the asset or object in the stage

---
+++ Assets/Item.cs
@@ -0,0 +1,44 @@
+﻿/*using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour {
+
+	// Use this for initialization
+	/*void Start () {
+		
+	}*/
+    /*private bool played = false;
+
+    void OnTriggerEnter(Collider col)
+    {
+        if(col.gameObject.tag == "play")
+        {
+            PickUp();
+        }
+    }
+	
+    public void PickUp()
+    {
+        Light PhoneLight = Camera.main.gameObject.GetComponent<Light>();
+        PhoneLight.MinusBattery();
+
+        if(played == true)
+        {
+            Object.Destroy(gameObject);
+        }
+    }
+
+    void OnGUI()
+    {
+        if (Event.current.keyCode == KeyCode.E)
+        {
+            played = true;
+            PickUp();
+        }
+    }
+	// Update is called once per frame
+	void Update () {
+
+    }
+}*/
---
+++ Assets/PhoneLight.cs
@@ -0,0 +1,74 @@
+﻿/*using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class PhoneLight : MonoBehaviour
+{
+
+    public float lightDrain = 0.1F;
+    public float batteryLife = 100;
+    public float maxbatteryLife = 100;
+    public Image currentBatteryLife;
+    public Text Batratio;
+
+    Light light;
+    // Use this for initialization
+    void Start()
+    {
+        light = GetComponent<Light>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (light.enabled == true && batteryLife >= 0)
+        {
+            batteryLife -= Time.deltaTime * lightDrain;
+            float ratio = batteryLife / maxbatteryLife;
+            currentBatteryLife.rectTransform.localScale = new Vector3(ratio, 1, 1);
+            Batratio.text = batteryLife.ToString() + '%';
+        }
+        if (light.enabled == false && batteryLife >= 0)
+        {
+            batteryLife += Time.deltaTime * lightDrain;
+            float ratio = batteryLife / maxbatteryLife;
+            currentBatteryLife.rectTransform.localScale = new Vector3(ratio, 1, 1);
+            Batratio.text = batteryLife.ToString() + '%';
+            if (batteryLife >= 100)
+            {
+                batteryLife = 100;
+            }
+        }
+        if (batteryLife <= 0)
+        {
+            batteryLife = 0;
+            light.enabled = false;
+            batteryLife += Time.deltaTime * lightDrain;
+            float ratio = batteryLife / maxbatteryLife;
+            currentBatteryLife.rectTransform.localScale = new Vector3(ratio, 1, 1);
+            Batratio.text = batteryLife.ToString() + '%';
+        }
+
+
+
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            if (light.enabled == true)
+            {
+                light.enabled = false;
+                Debug.Log("light is now false");
+
+            }
+            else if (light.enabled == false)
+            {
+                light.enabled = true;
+                Debug.Log("light is now true");
+
+            }
+
+        }
+    }
+}
+*/
+
