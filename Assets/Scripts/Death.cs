60ef666aa7928eab1c3059c1def0bd9897096b65
PartyTaylor <33302487+PartyTaylor@users.noreply.github.com>
2018-04-30 06:14:06 -0500

    Demo

---
+++ Assets/Scripts/Death.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Death : MonoBehaviour {
+    private Scene scene;
+
+    // Use this for initialization
+    void Start () {
+        scene = SceneManager.GetActiveScene();
+	}
+
+    // Update is called once per frame
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            SceneManager.LoadScene(1);
+        }
+    }
+}
---
+++ Assets/Scripts/TestFollow.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class TestFollow : MonoBehaviour
+{
+
+    public Transform player;
+
+    void Update()
+    {
+        NavMeshAgent agent = GetComponent<NavMeshAgent>();
+        agent.destination = player.position;
+    }
+}
