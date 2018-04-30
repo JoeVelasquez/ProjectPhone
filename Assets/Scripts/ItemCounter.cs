b23ea97c2a89c0d8fb002cbc601d99123de541c3
Jonie <37750408+moyajj172@users.noreply.github.com>
2018-04-29 23:13:44 -0500

    messages
    edit in item count script

--- Assets/Scripts/ItemCounter.cs
+++ Assets/Scripts/ItemCounter.cs
@@ -5,14 +5,15 @@ using UnityEngine;
 public class ItemCounter : MonoBehaviour {
 
     public int count = 0;
-    public int tcount = 4;
+    public int tcount = 0;
     private bool played = false;
     public bool playing = false;
-    
-	// Use this for initialization
-	void Start () {
-		
-	}
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
 
     public void OnTriggerEnter(Collider col)
     {
@@ -24,18 +25,30 @@ public class ItemCounter : MonoBehaviour {
 
     public void OnGUI()
     {
-        if (count < tcount)
+        if (tcount == 0)
         {
-            GUI.Box(new Rect((Screen.width / 2) - 100, 10, 200, 35), " " + count + " items found");
+            GUI.Box(new Rect((Screen.width / 2) - 100, 10, 200, 35), " Hurry and find all the items!");
         }
-        else
+        if (tcount == 1)
         {
-            GUI.Box(new Rect((Screen.width / 2) - 100, 10, 200, 35), "All the ritual items found.");
+            GUI.Box(new Rect((Screen.width / 2) - 100, 10, 250, 35), " " + count + " item found. Quickly find the next one!");
+        }
+        if (tcount == 2)
+        {
+            GUI.Box(new Rect((Screen.width / 2) - 100, 10, 300, 35), " " + count + " items found. The monster is getting closer!");
+        }
+        if (tcount == 3)
+        {
+            GUI.Box(new Rect((Screen.width / 2) - 100, 10, 200, 35), " " + count + " items found. Just one more!");
+        }
+        if (tcount == 4)
+        {
+            GUI.Box(new Rect((Screen.width / 2) - 100, 10, 350, 35), "You have found all the items! Hurry to the house!");
         }
     }
-	
-	// Update is called once per frame
-	void Update ()
+
+    // Update is called once per frame
+    void Update()
     {
         if (playing == true)
         {
@@ -46,6 +59,7 @@ public class ItemCounter : MonoBehaviour {
                     count = count + 1;
                     Debug.Log("You picked up a ritual item. Item = " + count);
                     played = true;
+                    tcount++;
                 }
                 played = false;
             }
