f55157634a66f8059d4800f40fb0288c03dd5e7c
Tobirenn <37786386+Tobirenn@users.noreply.github.com>
2018-04-30 01:42:05 -0500

    stamina script

---
+++ Assets/Scripts/Staminabar.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Staminabar : MonoBehaviour {
+
+    public float staminaDrain = 1F;
+    public float stamina = 100;
+    public float maxstamina = 100;
+    public Image currentstamina;
+
+    public bool run;
+
+    // Use this for initialization
+    void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            run = true; 
+        }
+        if(Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            run = false;
+        }
+
+        if (run == true && stamina >= 0)
+        {
+            stamina -= Time.deltaTime * staminaDrain;
+            float ratio = stamina / maxstamina;
+            currentstamina.rectTransform.localScale = new Vector3(ratio, 1, 1);
+        }
+        if (run == false && stamina >= 0)
+        {
+            stamina += Time.deltaTime * staminaDrain;
+            float ratio = stamina / maxstamina;
+            currentstamina.rectTransform.localScale = new Vector3(ratio, 1, 1);
+        }
+        if (stamina == 0)
+        {
+            run = false;
+            stamina -= Time.deltaTime * staminaDrain;
+            float ratio = stamina / maxstamina;
+            currentstamina.rectTransform.localScale = new Vector3(ratio, 1, 1);
+        }
+	}
+}
